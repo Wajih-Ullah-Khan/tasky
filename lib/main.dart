c6cffb8e2bff2bf2ea5c316ac68161d907a4b609
Wajih Ullah Khan <wajih2255@gmail.com>
2024-08-23 12:07:05 +0500

    Rewrite App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
-import 'package:flutter/material.dart';
+import'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
